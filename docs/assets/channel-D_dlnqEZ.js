a9266698606ca96ea2083f13c2bc2e2efd070b71
Kward <166002323+AllanVitu@users.noreply.github.com>
2026-05-08 14:38:00 +0200

    Debug all

--- docs/assets/channel-D_dlnqEZ.js
+++
@@ -1,1 +0,0 @@
-import{U as a,M as n}from"./ProjectUMLEditor-AMZX0x3o.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ docs/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/GestionnaireTDAH/sw.js', { scope: '/GestionnaireTDAH/' })})}
